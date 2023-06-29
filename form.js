5d6a8f0b9d02f96fbb30289f984cf849ab9cd30d
ThedEs2t <fedya_maer@mail.ru>
2023-06-29 22:15:20 +0600

    redacted

--- form.js
+++ form.js
@@ -1,3 +1,13 @@
-const button = document.getElementById('myButton');
+const anchors = document.querySelectorAll('a[href *="#"]')
 
-button.click();
+for(let anchor of anchors){
+    anchor.addEventListener("click", function(event){
+        event.preventDefault();
+
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
